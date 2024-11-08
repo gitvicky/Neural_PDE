c8d19d6a50118a282f0defe9848992de3ab0b0a2
Mr. Gopakumar <ir-gopa2@login-q-2.data.cluster>
2024-11-08 15:00:59 +0000

    gated MLP for sequences added

--- Models/ViT.py
+++ Models/ViT.py
@@ -6,136 +6,145 @@ import operator
 from functools import reduce
 
 
-# class PatchEmbedding(nn.Module):
-#     def __init__(self, img_size, patch_size, in_channels, embed_dim):
-#         super().__init__()
-#         self.img_size = img_size
-#         self.patch_size = patch_size
-#         self.grid_size = img_size // patch_size
-#         self.num_patches = self.grid_size ** 2
+class PatchEmbedding(nn.Module):
+    def __init__(self, img_size, patch_size, in_channels, embed_dim):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.grid_size = img_size // patch_size
+        self.num_patches = self.grid_size ** 2
         
-#         self.proj = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
+        self.proj = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
 
-#     def forward(self, x):
-#         x = self.proj(x)  # (B, embed_dim, H', W')
-#         x = x.flatten(2)  # (B, embed_dim, H'*W')
-#         x = x.transpose(1, 2)  # (B, H'*W', embed_dim)
-#         return x
+    def forward(self, x):
+        x = self.proj(x)  # (B, embed_dim, H', W')
+        print(f'Project: {x.shape}')
+        x = x.flatten(2)  # (B, embed_dim, H'*W')
+        x = x.transpose(1, 2)  # (B, H'*W', embed_dim)
+        return x
 
-# class Attention(nn.Module):
-#     def __init__(self, dim, n_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
-#         super().__init__()
-#         self.n_heads = n_heads
-#         self.scale = (dim // n_heads) ** -0.5
+class Attention(nn.Module):
+    def __init__(self, dim, n_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.n_heads = n_heads
+        self.scale = (dim // n_heads) ** -0.5
 
-#         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-#         self.attn_drop = nn.Dropout(attn_drop)
-#         self.proj = nn.Linear(dim, dim)
-#         self.proj_drop = nn.Dropout(proj_drop)
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
 
-#     def forward(self, x):
-#         B, N, C = x.shape
-#         qkv = self.qkv(x).reshape(B, N, 3, self.n_heads, C // self.n_heads).permute(2, 0, 3, 1, 4)
-#         q, k, v = qkv[0], qkv[1], qkv[2]
-#         attn = (q @ k.transpose(-2, -1)) * self.scale
-#         attn = attn.softmax(dim=-1)
-#         attn = self.attn_drop(attn)
-#         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-#         x = self.proj(x)
-#         x = self.proj_drop(x)
-#         return x     
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.n_heads, C // self.n_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        print(f'q: {q.shape}, k^t: {k.transpose(-2, -1).shape}, v: {v.shape}')
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x     
 
-# class TransformerBlock(nn.Module):
-#     def __init__(self, dim, n_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.):
-#         super().__init__()
-#         self.norm1 = nn.LayerNorm(dim)
-#         self.attn = Attention(dim, n_heads=n_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-#         self.norm2 = nn.LayerNorm(dim)
-#         mlp_hidden_dim = int(dim * mlp_ratio)
-#         self.mlp = nn.Sequential(
-#             nn.Linear(dim, mlp_hidden_dim),
-#             nn.GELU(),
-#             nn.Dropout(drop),
-#             nn.Linear(mlp_hidden_dim, dim),
-#             nn.Dropout(drop)
-#         )
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, n_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim)
+        self.attn = Attention(dim, n_heads=n_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        self.norm2 = nn.LayerNorm(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, mlp_hidden_dim),
+            nn.GELU(),
+            nn.Dropout(drop),
+            nn.Linear(mlp_hidden_dim, dim),
+            nn.Dropout(drop)
+        )
 
-#     def forward(self, x):
-#         x = x + self.attn(self.norm1(x))
-#         x = x + self.mlp(self.norm2(x))
-#         return x
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        print(f'attn: {x.shape}')
+        x = x + self.mlp(self.norm2(x))
+        print(f'mlp: {x.shape}')
+        return x
 
-# class VisionTransformer(nn.Module):
-#     def __init__(self, img_size=224, patch_size=16, in_channels=3, out_channels=3, embed_dim=768, depth=12,
-#                  n_heads=12, mlp_ratio=4., qkv_bias=True, drop_rate=0., attn_drop_rate=0.):
-#         super().__init__()
-#         self.patch_embed = PatchEmbedding(img_size, patch_size, in_channels, embed_dim)
-#         self.pos_embed = nn.Parameter(torch.zeros(1, self.patch_embed.num_patches, embed_dim))
-#         self.pos_drop = nn.Dropout(p=drop_rate)
+class VisionTransformer(nn.Module):
+    def __init__(self, img_size=224, patch_size=16, in_channels=3, out_channels=3, embed_dim=768, depth=12,
+                 n_heads=12, mlp_ratio=4., qkv_bias=True, drop_rate=0., attn_drop_rate=0.):
+        super().__init__()
+        self.patch_embed = PatchEmbedding(img_size, patch_size, in_channels, embed_dim)
+        self.pos_embed = nn.Parameter(torch.zeros(1, self.patch_embed.num_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
 
-#         self.blocks = nn.Sequential(*[
-#             TransformerBlock(embed_dim, n_heads, mlp_ratio, qkv_bias, drop_rate, attn_drop_rate)
-#             for _ in range(depth)
-#         ])
+        self.blocks = nn.Sequential(*[
+            TransformerBlock(embed_dim, n_heads, mlp_ratio, qkv_bias, drop_rate, attn_drop_rate)
+            for _ in range(depth)
+        ])
 
-#         self.norm = nn.LayerNorm(embed_dim)
-#         self.linear = nn.Linear(embed_dim, patch_size * patch_size * out_channels)
+        self.norm = nn.LayerNorm(embed_dim)
+        self.linear = nn.Linear(embed_dim, patch_size * patch_size * out_channels)
         
-#         self.img_size = img_size
-#         self.patch_size = patch_size
-#         self.out_channels = out_channels
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.out_channels = out_channels
 
-#     def forward(self, x):
-#         x = self.patch_embed(x)
-#         x = x + self.pos_embed
-#         x = self.pos_drop(x)
+    def forward(self, x):
+        x = self.patch_embed(x)
+        print(f'patch embed: {x.shape}')
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
 
-#         x = self.blocks(x)
-#         x = self.norm(x)
-#         x = self.linear(x)
-#         # Reshape to image
-#         B = x.shape[0]
-#         x = x.view(B, self.patch_embed.grid_size, self.patch_embed.grid_size, self.patch_size, self.patch_size, self.out_channels)
-#         x = x.permute(0, 5, 1, 3, 2, 4).contiguous()
-#         x = x.view(B, self.out_channels, self.img_size, self.img_size)
+        x = self.blocks(x)
+        x = self.norm(x)
+        x = self.linear(x)
+        # Reshape to image
+        B = x.shape[0]
+        x = x.view(B, self.patch_embed.grid_size, self.patch_embed.grid_size, self.patch_size, self.patch_size, self.out_channels)
+        x = x.permute(0, 5, 1, 3, 2, 4).contiguous()
+        x = x.view(B, self.out_channels, self.img_size, self.img_size)
 
-#         return x
+        return x
     
     
-#     def count_params(self):
-#         c = 0
-#         for p in self.parameters():
-#             c += reduce(operator.mul, list(p.size()))
+    def count_params(self):
+        c = 0
+        for p in self.parameters():
+            c += reduce(operator.mul, list(p.size()))
 
-#         return c
+        return c
 
-# # Example usage
-# img_size = 128
-# patch_size = 16
-# in_channels = 2
-# out_channels = 2
-# embed_dim = 768
-# depth = 12
-# n_heads = 12
+# Example usage
+img_size = 128
+patch_size = 16
+in_channels = 2
+out_channels = 2
+embed_dim = 768
+depth = 2
+n_heads = 4
 
-# model = VisionTransformer(
-#     img_size=img_size,
-#     patch_size=patch_size,
-#     in_channels=in_channels,
-#     out_channels=out_channels,
-#     embed_dim=embed_dim,
-#     depth=depth,
-#     n_heads=n_heads
-# )
+model = VisionTransformer(
+    img_size=img_size,
+    patch_size=patch_size,
+    in_channels=in_channels,
+    out_channels=out_channels,
+    embed_dim=embed_dim,
+    depth=depth,
+    n_heads=n_heads
+)
 
-# # Generate a random input tensor
-# x = torch.randn(1, in_channels, img_size, img_size)
+# Generate a random input tensor
+x = torch.randn(1, in_channels, img_size, img_size)
 
-# # Forward pass
-# output = model(x)
-# print(f"Input shape: {x.shape}")
-# print(f"Output shape: {output.shape}")
-# print(f"Paramters: {model.count_params()}")
+# Forward pass
+output = model(x)
+print(f"Input shape: {x.shape}")
+print(f"Output shape: {output.shape}")
+print(f"Paramters: {model.count_params()}")
+# %%
+
+
+
 # %%
 #Including both the variables and the time -- Batch, Variables, Nt, Nx, Ny
 
@@ -252,34 +261,34 @@ class VisionTransformer(nn.Module):
 
         return c
     
-# Example usage
-img_size = (128, 128)
-patch_size = (1, 16, 16) #Should be a factor of img_size. 
-in_channels = 2
-time_channels = 1
-out_channels = 2
-embed_dim = 128
-depth = 1
-n_heads = 1
+# # Example usage
+# img_size = (128, 128)
+# patch_size = (1, 16, 16) #Should be a factor of img_size. 
+# in_channels = 2
+# time_channels = 1
+# out_channels = 2
+# embed_dim = 128
+# depth = 1
+# n_heads = 1
 
-model = VisionTransformer(
-    img_size=img_size,
-    patch_size=patch_size,
-    in_channels=in_channels,
-    time_channels=time_channels,
-    out_channels=out_channels,
-    embed_dim=embed_dim,
-    depth=depth,
-    n_heads=n_heads
-)
+# model = VisionTransformer(
+#     img_size=img_size,
+#     patch_size=patch_size,
+#     in_channels=in_channels,
+#     time_channels=time_channels,
+#     out_channels=out_channels,
+#     embed_dim=embed_dim,
+#     depth=depth,
+#     n_heads=n_heads
+# )
 
-# Generate a random input tensor
-x = torch.randn(1, in_channels, img_size[0], img_size[1], time_channels)
+# # Generate a random input tensor
+# x = torch.randn(1, in_channels, img_size[0], img_size[1], time_channels)
 
-# Forward pass
-output = model(x)
-print(f"Input shape: {x.shape}")
-print(f"Output shape: {output.shape}")
-print(f"Paramters: {model.count_params()}")
+# # Forward pass
+# output = model(x)
+# print(f"Input shape: {x.shape}")
+# print(f"Output shape: {output.shape}")
+# print(f"Paramters: {model.count_params()}")
 
 # %%
---
+++ Models/gMLP.py
@@ -0,0 +1,159 @@
+"""
+---
+Code is from https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/transformers/gmlp/__init__.py
+---
+
+# Pay Attention to MLPs (gMLP)
+
+This is a [PyTorch](https://pytorch.org) implementation of the paper
+[Pay Attention to MLPs](https://arxiv.org/abs/2105.08050).
+
+This paper introduces a Multilayer Perceptron (MLP) based architecture with gating,
+which they name **gMLP**. It consists of a stack of $L$ *gMLP* blocks.
+
+"""
+
+# %% 
+from typing import Optional
+
+import torch
+from torch import nn
+
+
+class GMLPBlock(nn.Module):
+    """
+    ## gMLP Block
+
+    Each block does the following transformations to input embeddings
+    $X \in \mathbb{R}^{n \times d}$ where $n$ is the sequence length
+    and $d$ is the dimensionality of the embeddings:
+
+    \begin{align}
+    Z &= \sigma(XU) \\
+    \tilde{Z} &= s(Z) \\
+    Y &= \tilde{Z}V \\
+    \end{align}
+
+    where $V$ and $U$ are learnable projection weights.
+    $s(\cdot)$ is the Spacial Gating Unit defined below.
+    Output dimensionality of $s(\cdot)$ will be half of $Z$.
+    $\sigma$ is an activation function such as
+    [GeLU](https://pytorch.org/docs/stable/generated/torch.nn.GELU.html).
+    """
+
+    def __init__(self, d_model: int, d_ffn: int, seq_len: int):
+        """
+        * `d_model` is the dimensionality ($d$) of $X$
+        * `d_ffn` is the dimensionality of $Z$
+        * `seq_len` is the length of the token sequence ($n$)
+        """
+        super().__init__()
+        # Normalization layer fro Pre-Norm
+        self.norm = nn.LayerNorm([d_model])
+        # Activation function $\sigma$
+        self.activation = nn.GELU()
+        # Projection layer for $Z = \sigma(XU)$
+        self.proj1 = nn.Linear(d_model, d_ffn)
+        # Spacial Gating Unit $s(\cdot)$
+        self.sgu = SpacialGatingUnit(d_ffn, seq_len)
+        # Projection layer for $Y = \tilde{Z}V$
+        self.proj2 = nn.Linear(d_ffn // 2, d_model)
+        # Embedding size (required by [Encoder](../models.html#Encoder).
+        # We use the encoder module from transformer architecture and plug
+        # *gMLP* block as a replacement for the [Transformer Layer](../models.html#Encoder).
+        self.size = d_model
+
+    def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None):
+        """
+        * `x` is the input embedding tensor $X$ of shape `[seq_len, batch_size, d_model]`
+        * `mask` is a boolean mask of shape `[seq_len, seq_len, 1]` that controls the visibility of tokens
+         among each other.
+        """
+        # Keep a copy for shortcut connection
+        print(f'Input: {x.shape}')
+        shortcut = x
+        # Normalize $X$
+        x = self.norm(x)
+        # Projection and activation $Z = \sigma(XU)$
+        z = self.activation(self.proj1(x))
+        print(f'Proj1: {z.shape}')
+        # Spacial Gating Unit $\tilde{Z} = s(Z)$
+        z = self.sgu(z, mask)
+        # Final projection $Y = \tilde{Z}V$
+        z = self.proj2(z)
+
+        # Add the shortcut connection
+        return z + shortcut
+
+
+    def count_params(self):
+        nparams = 0
+
+        for param in self.parameters():
+            nparams += param.numel()
+        return nparams
+    
+    
+class SpacialGatingUnit(nn.Module):
+    """
+    ## Spatial Gating Unit
+
+    $$s(Z) = Z_1 \odot f_{W,b}(Z_2)$$
+
+    where $f_{W,b}(Z) = W Z + b$ is a linear transformation along the sequence dimension,
+    and $\odot$ is element-wise multiplication.
+    $Z$ is split into to parts of equal size $Z_1$ and $Z_2$ along the channel dimension (embedding dimension).
+    """
+    def __init__(self, d_z: int, seq_len: int):
+        """
+        * `d_z` is the dimensionality of $Z$
+        * `seq_len` is the sequence length
+        """
+        super().__init__()
+        # Normalization layer before applying $f_{W,b}(\cdot)$
+        self.norm = nn.LayerNorm([d_z // 2])
+        # Weight $W$ in $f_{W,b}(\cdot)$.
+        #
+        # The paper notes that it's important to initialize weights to small values and the bias to $1$,
+        # so that during the initial training $s(\cdot)$ is close to identity (apart from the split).
+        self.weight = nn.Parameter(torch.zeros(seq_len, seq_len).uniform_(-0.01, 0.01), requires_grad=True)
+        # Weight $b$ in $f_{W,b}(\cdot)$
+        #
+        # The paper notes that it's important to initialize bias to $1$.
+        self.bias = nn.Parameter(torch.ones(seq_len), requires_grad=True)
+
+    def forward(self, z: torch.Tensor, mask: Optional[torch.Tensor] = None):
+        """
+        * `z` is the input $Z$ of shape `[seq_len, batch_size, d_z]`
+        * `mask` is is a boolean mask of shape `[seq_len, seq_len, 1]` that controls the visibility of tokens
+         among each other. The last dimension of size `1` is the batch, which we have in other transformer
+         implementations and was left for compatibility.
+        """
+
+        # Get sequence length
+        seq_len = z.shape[0]
+        # Split $Z$ into $Z_1$ and $Z_2$
+        z1, z2 = torch.chunk(z, 2, dim=-1)
+        print(f'Channel_split1: {z1.shape}, Channel_split2: {z2.shape}') #Split along the Channels
+
+
+        # Normalize $Z_2$ before $f_{W,b}(\cdot)$
+        z2 = self.norm(z2)
+        # Get the weight matrix; truncate if larger than `seq_len`
+        weight = self.weight[:seq_len, :seq_len]
+
+        # $f_{W,b}(Z_2) = W Z_2 + b$
+        z2 = torch.einsum('ij,jbd->ibd', weight, z2) + self.bias[:seq_len, None, None]
+        print(f'spatial_proj: {z2.shape}')
+
+        # $Z_1 \odot f_{W,b}(Z_2)$
+        return z1 * z2
+    
+
+
+#Example Usage
+
+X = torch.ones(64, 1, 2) # seq_len, BS, ndim
+model = GMLPBlock(d_model=2, d_ffn=32, seq_len=64)
+out = model(X)
+# %%
